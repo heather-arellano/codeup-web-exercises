0bcafa42b6ffe19fd26fca3e080b3b565f89912d
heather arellano <hma.485@icloud.com>
2018-03-21 10:51:47 -0500

    Exercise Conditional JS Started first TODO completed

---
+++ js/extrafunctions.js
@@ -0,0 +1,66 @@
+// Write a function named identity that defines one parameter and returns the argument it was called with.
+function identity(name) {
+    return name
+
+}
+
+
+// Write a function named isOdd that accepts a number and returns true if the passed number is odd, otherwise false
+// function isOdd(number) {
+//     number%2==0
+//     return(number%2!=0)
+//
+// }
+// var odd= prompt("Pick a number");
+//  alert(isOdd(odd));
+
+//
+// Write a function named isEven that accepts a number and returns true if the passed number is even, false otherwise
+// function isEven(number) {
+//     return(number%2==0)
+// }
+// var even= prompt("Pick a number between 1-10");
+// alert(isEven(even));
+//
+// Write a function named isPositive that returns true if the passed number is greater than 0
+// function isPositive(number) {
+//     return (number >= 0)
+// }
+// var pickone=Number(prompt("pick a number"));
+// alert(isPositive(pickone));
+//
+// Write a function named decrement that accepts a number and returns the number minus 1
+//
+// function decrement(number){
+//     return(number-1)
+// }
+// var pickone=(prompt("pick a number"));
+// alert(decrement(pickone));
+
+// Write a function named hasSpaces that accepts a string and returns true if the string contains any space characters
+// function hasSpaces(string) {
+//     return(string.indexOf(" ")==-1);
+//
+// }
+// var word=(prompt("what is your favorite color?"));
+// alert(hasSpaces(word));
+
+//
+// Write a function named isFive that accepts a number and returns true if the number is equal to 5
+
+//
+// Write a function named square that accepts a number and returns the number times itself
+//
+// Write a function named cube that accepts a number and returns the number times itself times itself
+//
+// Write a function named half that accepts a number and returns the number halved
+//
+// Write a function named double that accepts a number and returns the number doubled
+//
+// Write a function named isValidPassword that accepts a string and
+// returns a boolean value based on the rules from the working
+// with data types exercise
+//
+// A triangle with sides a, b, and c is a right triangle iff a squared plus b squared is equal to c squared.Write a function named isRightTriangle with parameters a, b, and c
+// that will be numbers, and that tells if the passed sides can make a right triangle (returns a boolean).
+function isRightTriangle()
