5737a949215984bddb1c3cb324bf9678e66899a8
heather arellano <hma.485@icloud.com>
2018-03-22 16:58:27 -0500

    Exercise Loop JS started

---
+++ js/for_loops.js
@@ -0,0 +1,9 @@
+// for (var i = 1 ;i<=10; i++) {
+//     console.log("7x"+ i + "=" + 7*i);
+// }
+
+
+
+for ( var Math.random()20<=200;)  {
+    console.log(random + "is"random % 2);
+}
---
+++ js/while.js
@@ -0,0 +1,20 @@
+// var i = 2;
+//
+// while (i<=65536) {
+//     console.log(i);
+//     i *= 2;
+// }
+
+
+
+
+
+// var allCones = Math.floor(Math.random() * 50) + 50;
+//
+//
+// do { alert(numbers + " cones sold");
+//     var numbers= Math.floor(Math.random() * 5) + 1;
+//     allCones -= numbers;
+//
+// } while ( allCones >=0);
+
